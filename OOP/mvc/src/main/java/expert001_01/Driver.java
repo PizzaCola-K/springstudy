9280c1012b8e9767494cb4bcdc0bca14b943a29b
K <PizzaCola.K@gmail.com>
2021-01-22 21:55:16 +0900

    Spring OOP: Dependency Injection by Spring xml

--- OOP/mvc/src/main/java/expert001_01/Driver.java
+++ OOP/mvc/src/main/java/expert001_01/Driver.java
@@ -1,9 +1,15 @@
 package expert001_01;
 
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Driver {
     public static void main(String[] args) {
-        Tire tire = new KoreaTire();
-        Car car = new Car();
+        ApplicationContext context = new ClassPathXmlApplicationContext("expert001_01/expert001_01.xml");
+        Car car = context.getBean("car", Car.class);
+        Tire tire = context.getBean("tire", Tire.class);
+
         car.setTire(tire);
         System.out.println(car.getTireBrand());
     }

3384dbaf0ab9731b4f98e198fcda2541dc4724a1
K <PizzaCola.K@gmail.com>
2021-01-27 16:52:00 +0900

    Spring5fs: multiple config

--- spring5fs/sp5-chap03/src/main/java/main/MainForSpring.java
+++ spring5fs/sp5-chap03/src/main/java/main/MainForSpring.java
@@ -1,7 +1,7 @@
 package main;
 
-import assembler.Assembler;
-import config.AppCtx;
+import config.AppConf1;
+import config.AppConf2;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.*;
@@ -15,7 +15,7 @@ public class MainForSpring {
     private static ApplicationContext ctx = null;
 
     public static void main(String[] args) throws IOException {
-        ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+        ctx = new AnnotationConfigApplicationContext(AppConf1.class, AppConf2.class);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             System.out.println("명령어를 입력하세요.");

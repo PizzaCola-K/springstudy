37b626a2a5f85efaa1493bf9aa7b4c1ded5276dc
K <PizzaCola.K@gmail.com>
2021-01-29 15:48:41 +0900

    Spring5fs: Autowired 01

---
+++ spring5fs/sp5-chap04/src/main/java/config/AppCtx.java
@@ -0,0 +1,47 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.*;
+
+@Configuration
+public class AppCtx {
+
+    @Bean
+    public MemberDao memberDao() {
+        return new MemberDao();
+    }
+
+    @Bean
+    public MemberPrinter memberPrinter() {
+        return new MemberPrinter();
+    }
+
+    @Bean
+    public MemberRegisterService memberRegSvc() {
+        return new MemberRegisterService();
+    }
+
+    @Bean
+    public ChangePasswordService changePwdSvc() {
+        return new ChangePasswordService();
+    }
+
+    @Bean
+    public MemberListPrinter listPrinter() {
+        return new MemberListPrinter();
+    }
+
+    @Bean
+    public MemberInfoPrinter infoPrinter() {
+        return new MemberInfoPrinter();
+    }
+
+    @Bean
+    public VersionPrinter versionPrinter() {
+        VersionPrinter versionPrinter = new VersionPrinter();
+        versionPrinter.setMajorVersion(5);
+        versionPrinter.setMinorVersion(0);
+        return versionPrinter;
+    }
+}
---
+++ spring5fs/sp5-chap04/src/main/java/spring/MemberInfoPrinter.java
@@ -0,0 +1,30 @@
+package spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberInfoPrinter {
+
+    private MemberDao memberDao;
+
+    private MemberPrinter memberPrinter;
+
+    public void printMemberInfo(String email) {
+        Member member = memberDao.selectByEmail(email);
+        if (member == null) {
+            System.out.println("데이터 없음");
+        } else {
+            memberPrinter.print(member);
+        }
+        System.out.println();
+    }
+
+    @Autowired
+    public void setMemberDao(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @Autowired
+    public void setMemberPrinter(MemberPrinter memberPrinter) {
+        this.memberPrinter = memberPrinter;
+    }
+}

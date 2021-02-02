40247077dfd79cb575ba1482502214a38ac0a722
K <PizzaCola.K@gmail.com>
2021-02-02 16:57:29 +0900

    Spring5fs: Logging

--- spring5fs/sp5-chap08/src/main/java/config/AppCtx.java
+++ spring5fs/sp5-chap08/src/main/java/config/AppCtx.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import spring.ChangePasswordService;
 import spring.MemberDao;
 
 @Configuration
+@EnableTransactionManagement
 public class AppCtx {
 
     @Bean(destroyMethod = "close")
--- spring5fs/sp5-chap08/src/main/java/main/MainForCPS.java
+++ spring5fs/sp5-chap08/src/main/java/main/MainForCPS.java
@@ -14,7 +14,7 @@ public class MainForCPS {
         ChangePasswordService cps = ctx.getBean("changePwdSvc", ChangePasswordService.class);
 
         try {
-            cps.changePassword("madvirus@madvirus.net", "1234", "1111");
+            cps.changePassword("madvirus@madvirus.net", "0x1.da9513e7d559p-1", "1111");
         } catch (MemberNotFoundException | WrongIdPasswordException e) {
             e.printStackTrace();
         }

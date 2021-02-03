185590391125da081c98787d63d46ff2827d687f
K <PizzaCola.K@gmail.com>
2021-02-03 16:47:34 +0900

    Spring5fs: Add register step3
    Link command object and spring form.

--- spring5fs/sp5-chap11/src/main/java/controller/RegisterController.java
+++ spring5fs/sp5-chap11/src/main/java/controller/RegisterController.java
@@ -1,13 +1,20 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import spring.DuplicateMemberException;
+import spring.MemberRegisterService;
+import spring.RegisterRequest;
 
 @Controller
 public class RegisterController {
+
+    private MemberRegisterService memberRegisterService;
+
+    public void setMemberRegisterService(MemberRegisterService memberRegisterService) {
+        this.memberRegisterService = memberRegisterService;
+    }
 
     @RequestMapping("/register/step1")
     public String handleStep1() {
@@ -16,11 +23,13 @@ public class RegisterController {
 
     @PostMapping("/register/step2")
     public String handleStep2(
-            @RequestParam(value = "agree", defaultValue = "false") Boolean agree
+            @RequestParam(value = "agree", defaultValue = "false") Boolean agree,
+            Model model
     ) {
         if (!agree) {
             return "redirect:/register/step1";
         } else {
+            model.addAttribute("registerData", new RegisterRequest());
             return "register/step2";
         }
     }
@@ -28,5 +37,15 @@ public class RegisterController {
     @GetMapping("/register/step2")
     public String handleStep2Get() {
         return "redirect:/register/step1";
+    }
+
+    @PostMapping("/register/step3")
+    public String handleStep3(@ModelAttribute("registerData") RegisterRequest regReq) {
+        try {
+            memberRegisterService.regist(regReq);
+            return "register/step3";
+        } catch (DuplicateMemberException ex) {
+            return "register/step2";
+        }
     }
 }
